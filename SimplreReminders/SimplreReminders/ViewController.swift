11b45fefb1649d549800f3417a4d4a407ce4fab8
Aliaksandr Baranouski <al.baranovski89@gmailcom>
2019-11-09 17:58:38 +0100

    Integrate dependencies via SPM

---
+++ SimplreReminders/SimplreReminders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimplreReminders
+//
+//  Created by Aliaksandr Baranouski on 09/11/2019.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
